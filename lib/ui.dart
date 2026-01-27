285c30083538aea7d09c30c8b77e48feb348e16a
Егор <tujhustify@gmail.com>
2026-01-27 23:20:28 +0300

    Category, NavBar, Cards
    Components for home screen, fixed image in slider cards. Added bottom navigation bar.

--- lib/ui.dart
+++ lib/ui.dart
@@ -221,10 +221,7 @@ class PromoCard extends StatelessWidget {
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
         gradient: const LinearGradient(
-          colors: [
-            Color(0xFF7AD7E8),
-            Color(0xFFA7EDE7),
-          ],
+          colors: [Color(0xFF7AD7E8), Color(0xFFA7EDE7)],
         ),
       ),
       child: Row(
@@ -253,10 +250,161 @@ class PromoCard extends StatelessWidget {
               ],
             ),
           ),
-          //TODO: if image equals null -> error
-          Image.asset(image!)
+          ?image != null ? Image.asset(image!) : null,
         ],
       ),
+    );
+  }
+}
+
+class Category extends StatelessWidget {
+  final Function() func;
+  final String title;
+  final bool isSelected;
+
+  const Category({
+    super.key,
+    required this.func,
+    required this.title,
+    this.isSelected = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+      style: FilledButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        backgroundColor: isSelected ? Colors.blueAccent : Color(0xFFF5F5F9),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 14),
+      ),
+      onPressed: func,
+      child: Text(
+        title,
+        style: TextStyle(color: isSelected ? Colors.white : Color(0xFF7E7E9A)),
+      ),
+    );
+  }
+}
+
+class CardBackground extends StatelessWidget {
+  const CardBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 140,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+        border: BoxBorder.all(color: Color(0xFFF4F4F4)),
+      ),
+      padding: EdgeInsets.all(14),
+      child: CardOrder(),
+    );
+  }
+}
+
+// Primary
+class CardOrder extends StatelessWidget {
+  const CardOrder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          'Рубашка Воскресенье для машинного вязания',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Мужская одежда',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Color(0xFF939396),
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  '300 ₽',
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+            //TODO: Move small button object to separate class || changed of state (add/added)
+            FilledButton(
+              onPressed: () {},
+              style: FilledButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                backgroundColor: Colors.blueAccent,
+                padding: EdgeInsets.symmetric(horizontal: 15.5, vertical: 10),
+              ),
+              child: Text('Добавить'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+// TabBar
+class TabNavigation extends StatelessWidget {
+  final List<Map<String, dynamic>> items;
+  final int selectedIndex;
+  final ValueChanged<int> func;
+
+  const TabNavigation({
+    super.key,
+    required this.items,
+    required this.selectedIndex,
+    required this.func,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: List.generate(items.length, (index) {
+        return Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border(top: BorderSide(color: Colors.grey.shade200)),
+            ),
+            child: InkWell(
+              onTap: () => func(index),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    items[index]['icon'],
+                    color: selectedIndex == index
+                        ? Colors.blueAccent
+                        : Colors.grey,
+                  ),
+                  Text(
+                    items[index]['title'],
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: selectedIndex == index
+                          ? Colors.blueAccent
+                          : Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
